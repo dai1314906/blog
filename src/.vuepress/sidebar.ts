309ef29f49b6bb399a299bd9d39b8dfaa0c4c74b
dai1314906 <dailin359@gmail.com>
2025-08-05 21:22:47 +0800

    博客新增面经页面

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -68,6 +68,43 @@ export default sidebar({
       ],
     },
     {
+      text: "Java高频八股文",
+      icon: "book",
+      prefix: "Java高频八股文/",
+      collapsible: true,
+      children: [
+        {
+          text: "MySQL篇",
+          icon: "book",
+          prefix: "MySQL篇/",
+          collapsible: true,
+          children: [
+              {
+                text: "索引问题",
+                prefix: "索引问题/",
+                collapsible: true,
+                children: "structure",
+              },
+              {
+                text: "锁问题",
+                prefix: "锁问题/",
+                collapsible: true,
+                children: [
+                  {
+                    text: "乐观锁与悲观锁",
+                    link: "乐观锁与悲观锁",
+                  },
+                  {
+                    text: "排它锁与互斥锁",
+                    link: "排它锁与互斥锁",
+                  }
+                ],
+              },
+          ],
+        }
+      ],
+    },
+    {
       text: "算法",
       icon: "book",
       prefix: "算法/",
@@ -75,32 +112,33 @@ export default sidebar({
       children: [
         {
           text: "左程云算法学习",
+          icon: "book",
           prefix: "左程云算法学习/",
           collapsible: true,
           children: [
-              {
-                text: "01经典排序",
-                prefix: "01经典排序/",
-                collapsible: true,
-                children: "structure",
-              },
-              {
-                text: "02链表",
-                prefix: "02链表/",
-                collapsible: true,
-                children: [
-                  {
-                    text: "判断单链表是否为回文结构",
-                    link: "判断单链表是否为回文结构",
-                  }
-                ],
-              },
-              {
-                text: "03二叉树",
-                prefix: "03二叉树/",
-                collapsible: true,
-                children: "structure",
-              },
+            {
+              text: "01经典排序",
+              prefix: "01经典排序/",
+              collapsible: true,
+              children: "structure",
+            },
+            {
+              text: "02链表",
+              prefix: "02链表/",
+              collapsible: true,
+              children: [
+                {
+                  text: "判断单链表是否为回文结构",
+                  link: "判断单链表是否为回文结构",
+                }
+              ],
+            },
+            {
+              text: "03二叉树",
+              prefix: "03二叉树/",
+              collapsible: true,
+              children: "structure",
+            },
           ],
         }
       ],
